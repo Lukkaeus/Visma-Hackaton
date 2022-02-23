02de2d97b4e59326160f4cdbfdfceb36a15605b3
lukas.tkacik <lukas.tkacik@SK-KOS-SC-0118>
2022-02-23 15:53:54 +0100

    Add project files.

---
+++ Hackaton/Controllers/TaskController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Hackaton.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public TaskController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select TaskId,TaskName,TaskDescription,DateOfCreation,DateOfExpiration,ExpectedDuration from dbo.Task";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TaskAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+
+    }
+}
---
+++ Hackaton/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿namespace Hackaton.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public string DateOfCreation { get; set; }
+        public string DateOfExpiration { get; set; }
+        public string ExpectedDuration { get; set; }
+
+
+    }
+}
