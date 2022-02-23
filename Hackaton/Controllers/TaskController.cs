913c9b5117a2ce569d8e6d8f5b121d0f91816538
lukas.tkacik <lukas.tkacikk@gmail.com>
2022-02-23 16:55:48 +0100

    HTTP Get,Post,Put

--- Hackaton/Controllers/TaskController.cs
+++ Hackaton/Controllers/TaskController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Hackaton.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Data;
 using System.Data.SqlClient;
+
 
 namespace Hackaton.Controllers
 {
@@ -21,7 +23,15 @@ namespace Hackaton.Controllers
         [HttpGet]
         public JsonResult Get()
         {
-            string query = @"select TaskId,TaskName,TaskDescription,DateOfCreation,DateOfExpiration,ExpectedDuration from dbo.Task";
+            string query = @"SELECT TaskId,
+                                    TaskName,
+                                    TaskDescription,
+                                    convert(varchar(10),DateOfCreation,120) as DateOfCreation,
+                                    convert(varchar(10),DateOfExpiration,120) as DateOfExpiration,
+                                    ExpectedDuration
+                             FROM
+                                    dbo.Task";
+
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("TaskAppCon");
             SqlDataReader myReader;
@@ -38,6 +48,69 @@ namespace Hackaton.Controllers
                 }
             }
             return new JsonResult(table);
+        }
+
+        [HttpPost]
+
+        public JsonResult Post(Task task)
+        {
+            string query = @"INSERT INTO 
+                                    dbo.Task (TaskName,TaskDescription,DateOfCreation,DateOfExpiration,ExpectedDuration)
+                            VALUES
+                            (
+                            '" + task.TaskName + @"'
+                            ,'" + task.TaskDescription + @"'
+                            ,'" + task.DateOfCreation + @"'
+                            ,'" + task.DateOfExpiration + @"'
+                            ,'" + task.ExpectedDuration + @"'
+                            )";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TaskAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added succesfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Task task)
+        {
+            string query = @"UPDATE dbo.Task SET 
+                            TaskName = '" + task.TaskName + @"'
+                            ,TaskDescription = '" + task.TaskDescription + @"'
+                            ,DateOfCreation = '" + task.DateOfCreation + @"'
+                            ,DateOfExpiration = '" + task.DateOfExpiration + @"'
+                            ,ExpectedDuration = '" + task.ExpectedDuration + @"'
+                            where TaskId = " + task.TaskId + @"
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TaskAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated succesfully");
         }
 
 
